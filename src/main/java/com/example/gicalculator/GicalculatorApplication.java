6c1089f6ddecf5f64fe5c64458f350e48ecfa911
GustavoHenriqueSC <gustavohenriquesilvacampos@gmail.com>
2023-11-21 04:27:51 -0300

    Initial change
    Adding the initial folder to github

---
+++ src/main/java/com/example/gicalculator/GicalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.gicalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GicalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GicalculatorApplication.class, args);
+	}
+
+}
